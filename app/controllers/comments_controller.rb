f7d777a5289dddf25255f41e228688ae6883f0c3
leandrobelge <leandrobelge@gmail.com>
2013-11-19 20:11:49 -0200

    Commit parte de Ajax

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  def create
+    @job = Job.find(params[:job_id])
+    @comment = @job.comments.build(params[:comment])
+    
+    respond_to do |format|
+      
+      format.html do    
+        if @comment.save
+          flash[:notice] = "Comment was created with success!"
+        else
+          flash[:alert] = "Please fill in all fields to create a comment."
+        end
+        redirect_to @job
+      end
+      
+      format.js do      
+        @comment.save    
+      end 
+    end    
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @comment.job, notice: "Comment destroyed with success."
+  end
+end
